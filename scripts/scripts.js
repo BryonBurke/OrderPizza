9fd14bd0d7231614781242c2f497de2ee5e6f7b1
Bryon Burke <bryonparis@gmail.com>
2019-11-08 09:33:22 -0800

    add toppings array and set up pizza object

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,15 +2,33 @@ $(document).ready(function(){
   $("form#pizzaToppings").submit(function(event){
     event.preventDefault();
 
+    function Pizza(toppingsArray) {
+      this.toppingsArray = toppingsArray,
+    }
+
+    Pizza.prototype.getPrice = function();
+
+
+
+
+
+
+
+
+
+    let toppingsArray = [];
+
     $("#toppingsSelected").show();
     $("input:checkbox[name=pizza-toppings]:checked").each(function(){
       var pizzaToppingsSelected = $(this).val();
-      let toppingArray = [];
-      toppingArray.push
+      toppingsArray.push(pizzaToppingsSelected);
       $('#toppingsSelected').append(pizzaToppingsSelected + "<br>");
 
-      console.log(pizzaToppingsSelected);
     });
+    console.log(toppingArray);
 
   });
 });
+
+let myPizza = new Pizza (toppingsArray)
+let price = myPizza.getPrice
