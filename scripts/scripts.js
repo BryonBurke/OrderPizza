1a1161940a1ed8b1c3c059422031dbed363f0554
Bryon Burke <bryonparis@gmail.com>
2019-11-08 14:31:25 -0800

    continue to add veg form

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,38 +1,50 @@
 $(document).ready(function(){
+
+function Pizza(meatsArray, vegArray) {
+  this.meatsArray = meatsArray
+  this.vegArray = vegArray
+}
+
+Pizza.prototype.getPrice = function(){
+  let pizzaPrice = pizzaBasePrice + (meatsArray.length * 1.50);
+  return pizzaPrice;
+}
+
+  let pizzaBasePrice = 10;
+  let meatPrice = 2;
+  let vegPrice =1;
+  let meatsArray = [];
+  let vegsArray = [];
+
   $("form#meatToppings").submit(function(event){
     event.preventDefault();
-
-    let pizzaBasePrice = 10;
-    let meatPrice = 2;
-    let vegPrice =1;
-    let meatsArray = [];
-
-    function Pizza(meatsArray) {
-      this.meatsArray = meatsArray
-    }
-
-    Pizza.prototype.getPrice = function(){
-      let pizzaPrice = pizzaBasePrice + (meatsArray.length * 1.50);
-      return pizzaPrice;
-    }
-
-
-
     $("#toppingsSelected").show();
     $("input:checkbox[name=meat-toppings]:checked").each(function(){
       var meatToppingsSelected = $(this).val();
       meatsArray.push(meatToppingsSelected);
       $('#toppingsSelected').append(meatToppingsSelected + "<br>");
-
     });
-
-
-    let myPizza = new Pizza (meatsArray);
-    let price = myPizza.getPrice().toFixed(2);
-
-    $("#yourPrice").text(price);
-    $("#showPrice").show();
-
-  console.log(price);
   });
+
+
+  $("form#vegToppings").submit(function(event){
+    event.preventDefault();
+    $("#toppingsSelected").show();
+    $("input:checkbox[name=veg-toppings]:checked").each(function(){
+      var vegToppingsSelected = $(this).val();
+      vegsArray.push(vegToppingsSelected);
+      $('#toppingsSelected').append(meatToppingsSelected + "<br>");
+    });
+  });
+
+
+  let myPizza = new Pizza (meatsArray, vegsArray);
+
+console.log(meatsArray);
+
+  let price = myPizza.getPrice().toFixed(2);
+
+  $("#yourPrice").text(price);
+  $("#showPrice").show();
+
 });
