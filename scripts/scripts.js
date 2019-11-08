00ad88a0618ec4a8b2cb46508630c59f521862c5
Bryon Burke <bryonparis@gmail.com>
2019-11-08 15:00:06 -0800

    continue veg form

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,50 +1,47 @@
 $(document).ready(function(){
-
-function Pizza(meatsArray, vegArray) {
-  this.meatsArray = meatsArray
-  this.vegArray = vegArray
-}
-
-Pizza.prototype.getPrice = function(){
-  let pizzaPrice = pizzaBasePrice + (meatsArray.length * 1.50);
-  return pizzaPrice;
-}
-
   let pizzaBasePrice = 10;
   let meatPrice = 2;
   let vegPrice =1;
   let meatsArray = [];
   let vegsArray = [];
 
-  $("form#meatToppings").submit(function(event){
-    event.preventDefault();
-    $("#toppingsSelected").show();
-    $("input:checkbox[name=meat-toppings]:checked").each(function(){
-      var meatToppingsSelected = $(this).val();
-      meatsArray.push(meatToppingsSelected);
-      $('#toppingsSelected').append(meatToppingsSelected + "<br>");
+  function Pizza(meatsArray, vegArray) {
+    this.meatsArray = meatsArray
+    this.vegArray = vegArray
+  }
+
+  Pizza.prototype.getPrice = function(){
+    let pizzaPrice = pizzaBasePrice + (meatsArray.length * meatPrice);
+    return pizzaPrice;
+  }
+
+
+    $("form#meatToppings").submit(function(event){
+      event.preventDefault();
+      $("#toppingsSelected").show();
+      $("input:checkbox[name=meat-toppings]:checked").each(function(){
+        let meatToppingsSelected = $(this).val();
+        meatsArray.push(meatToppingsSelected);
+        $('#toppingsSelected').append(meatToppingsSelected + "<br>");
+      });
+
     });
-  });
+
+    // $("form#vegToppings").submit(function(event){
+    //   event.preventDefault();
+    //   $("#toppingsSelected").show();
+    //   $("input:checkbox[name=veg-toppings]:checked").each(function(){
+    //     let vegToppingsSelected = $(this).val();
+    //     vegsArray.push(vegToppingsSelected);
+    //     $('#toppingsSelected').append(meatToppingsSelected + "<br>");
+    //   });
+    // });
 
 
-  $("form#vegToppings").submit(function(event){
-    event.preventDefault();
-    $("#toppingsSelected").show();
-    $("input:checkbox[name=veg-toppings]:checked").each(function(){
-      var vegToppingsSelected = $(this).val();
-      vegsArray.push(vegToppingsSelected);
-      $('#toppingsSelected').append(meatToppingsSelected + "<br>");
-    });
-  });
-
-
-  let myPizza = new Pizza (meatsArray, vegsArray);
-
-console.log(meatsArray);
-
-  let price = myPizza.getPrice().toFixed(2);
-
-  $("#yourPrice").text(price);
-  $("#showPrice").show();
+    let myPizza = new Pizza (meatsArray, vegsArray);
+    console.log(meatsArray);
+    let price = myPizza.getPrice().toFixed(2);
+    $("#yourPrice").text(price);
+    $("#showPrice").show();
 
 });
