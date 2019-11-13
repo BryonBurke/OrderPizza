4d64aebd99d46215b0866510ad6b5801ccac52bb
Bryon Burke <bryonparis@gmail.com>
2019-11-12 19:07:49 -0800

    separate ui and business logic. Edit readme to conform to spec

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,9 @@
-
 // Business logic
+    let pizzaBasePrice = 10;
+    let meatPrice = 2;
+    let vegPrice =1;
+    let meatArray = [];
+    let vegArray = [];
     function Pizza(meatArray, vegArray) {
       this.meatArray = meatArray
       this.vegArray = vegArray
@@ -8,7 +12,12 @@
       let pizzaPrice = pizzaBasePrice + (meatArray.length * meatPrice) + (vegArray.length * vegPrice);
       return pizzaPrice;
     }
-    $("#toppingsSelected").show();
+
+    // User Interface logic
+
+    $(document).ready(function(){
+      $("form#pizzaToppings").submit(function(event){
+        event.preventDefault();
     // collect meat items and push to object meat array
     $("input:checkbox[name=meat-toppings]:checked").each(function(){
       var pizzaToppingsSelected = $(this).val();
@@ -20,21 +29,8 @@
       var pizzaToppingsSelected = $(this).val();
       vegArray.push(pizzaToppingsSelected);
       $('#toppingsSelected').append(pizzaToppingsSelected + "<br>");
+      $("#toppingsSelected").show();
     });
-
-
-
-// User Interface logic
-
-$(document).ready(function(){
-  $("form#pizzaToppings").submit(function(event){
-    event.preventDefault();
-
-    let pizzaBasePrice = 10;
-    let meatPrice = 2;
-    let vegPrice =1;
-    let meatArray = [];
-    let vegArray = [];
 
     let myPizza = new Pizza (meatArray, vegArray);
     let price = myPizza.getPrice().toFixed(2);
