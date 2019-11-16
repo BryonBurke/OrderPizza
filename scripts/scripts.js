a3d06ec0ad1ab9982725fff52d8a279d5d883fe2
BryonBurke <bryonparis@gmail.com>
2019-11-15 19:33:47 -0800

    update object structure

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,44 +1,47 @@
 // Business logic
 
-function Pizza(pizzaSize, meatArray, vegArray) {
+function Pizza(basePrice, meatArray, vegArray) {
+  // this.basePrice = basePrice
+  this.basePrice = basePrice
   this.meatArray = meatArray
   this.vegArray = vegArray
-  this.pizzaSize = pizzaSize
-  this.price = 10
+
 }
 Pizza.prototype.getPrice = function(){
-  if (this.pizzaSize === "small"){
-    basePrice = 10;
-  }
-  else if (this.pizzaSize === "medium") {
-    basePrice = 15;
-  }
-  else if (this.pizzaSize === "large") {
-    basePrice = 20;
-  }
-  this.price = basePrice + (meatArray.length * 2) + (vegArray.length);
+
+  this.price = this.basePrice + (this.meatArray.length * 2) + (this.vegArray.length);
+  // this.price = 19;
   return this.price
+
+
+
+  // return this.price
 }
 // User Interface logic
 $(document).ready(function(){
   $("form#pizzaToppings").submit(function(event){
     event.preventDefault();
+    var pizzaSize = $("#pizzaSizeSelecter").val();
+    var basePrice = (parseInt(pizzaSize));
     var meatArray = [];
     var vegArray = [];
-    var pizzaSize = $("#pizzaSizeSelecter").val();
+
+
     // collect meat items and push to object meat array
     $("input:checkbox[name=meat-toppings]:checked").each(function(){
       var pizzaToppingsSelected = $(this).val();
       meatArray.push(pizzaToppingsSelected);
       $('#toppingsSelected').append(pizzaToppingsSelected + "<br>");
     });
+    console.log(meatArray);
     // collect veg items and push to veg array
     $("input:checkbox[name=veg-toppings]:checked").each(function(){
       var pizzaToppingsSelected = $(this).val();
       vegArray.push(pizzaToppingsSelected);
       $('#toppingsSelected').append(pizzaToppingsSelected + "<br>");
     });
-    var myPizza = new Pizza (pizzaSize, meatArray, vegArray);
+    var myPizza = new Pizza (basePrice, meatArray, vegArray);
+
     var price = myPizza.getPrice();
     $("#toppingsSelected").show();
     $("#myPrice").text(price);
