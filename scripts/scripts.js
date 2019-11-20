dbacec966124b837e09f3fd00837aaef1a43342f
bryon burke <bryonburke@bryons-Mac-mini.local>
2019-11-19 21:30:22 -0800

    refactor and properly use object properties

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,13 @@
 // Business logic
 function Pizza(basePrice, meatArray, vegArray) {
-  // this.basePrice = basePrice
   this.basePrice = basePrice
   this.meatArray = meatArray
   this.vegArray = vegArray
+
+
 }
 Pizza.prototype.getPrice = function(){
   this.price = this.basePrice + (this.meatArray.length * 2) + (this.vegArray.length);
-  return this.price
 }
 
 // User Interface logic
@@ -21,18 +21,22 @@ $(document).ready(function(){
     $("input:checkbox[name=meat-toppings]:checked").each(function(){
       var pizzaToppingsSelected = $(this).val();
       meatArray.push(pizzaToppingsSelected);
-      $('#toppingsSelected').append(pizzaToppingsSelected + "<br>");
     });
     console.log(meatArray);
+
     $("input:checkbox[name=veg-toppings]:checked").each(function(){
       var pizzaToppingsSelected = $(this).val();
       vegArray.push(pizzaToppingsSelected);
-      $('#toppingsSelected').append(pizzaToppingsSelected + "<br>");
+
     });
     var myPizza = new Pizza (basePrice, meatArray, vegArray);
-    var price = myPizza.getPrice();
+    myPizza.getPrice();
+
     $("#toppingsSelected").show();
-    $("#myPrice").text(price);
+    $("#meat").text(myPizza.meatArray);
+    $("#veg").text(myPizza.vegArray);
+
     $("#showPrice").show();
+    $("#myPrice").text(myPizza.price);
   });
 });
