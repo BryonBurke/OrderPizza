a174ee3b6e7741073f470cc6c28e62b90c9f09eb
bryon burke <bryonburke@bryons-Mac-mini.local>
2019-11-19 21:59:22 -0800

    Edit for readability

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,8 +3,6 @@ function Pizza(basePrice, meatArray, vegArray) {
   this.basePrice = basePrice
   this.meatArray = meatArray
   this.vegArray = vegArray
-
-
 }
 Pizza.prototype.getPrice = function(){
   this.price = this.basePrice + (this.meatArray.length * 2) + (this.vegArray.length);
@@ -22,20 +20,15 @@ $(document).ready(function(){
       var pizzaToppingsSelected = $(this).val();
       meatArray.push(pizzaToppingsSelected);
     });
-    console.log(meatArray);
-
     $("input:checkbox[name=veg-toppings]:checked").each(function(){
       var pizzaToppingsSelected = $(this).val();
       vegArray.push(pizzaToppingsSelected);
-
     });
     var myPizza = new Pizza (basePrice, meatArray, vegArray);
     myPizza.getPrice();
-
     $("#toppingsSelected").show();
     $("#meat").text(myPizza.meatArray);
     $("#veg").text(myPizza.vegArray);
-
     $("#showPrice").show();
     $("#myPrice").text(myPizza.price);
   });
